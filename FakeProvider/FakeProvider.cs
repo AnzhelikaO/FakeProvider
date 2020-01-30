dbd99893b279e0780b45831961f174f9a4fc2694
ASgoPew <35419712+ASgoPew@users.noreply.github.com>
2020-01-30 21:57:35 +0300

    Custom world loading

--- FakeProvider/FakeProvider.cs
+++ FakeProvider/FakeProvider.cs
@@ -2,11 +2,16 @@
 using OTAPI;
 using OTAPI.Tile;
 using System;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
 using Terraria;
+using Terraria.IO;
+using Terraria.Social;
+using Terraria.Utilities;
 using TerrariaApi.Server;
+using OTAPI.Callbacks.Terraria;
 #endregion
 namespace FakeProvider
 {
@@ -124,10 +129,11 @@ namespace FakeProvider
             for (int i = 0; i < Main.maxPlayers; i++)
                 AllPlayers[i] = i;
 
+            Hooks.World.IO.PreLoadWorld += OnPreLoadWorld;
+            //Hooks.World.IO.PostLoadWorld += OnPostLoadWorld;
+            Hooks.World.IO.PreSaveWorld += OnPreSaveWorld;
+            Hooks.World.IO.PostSaveWorld += OnPostSaveWorld;
             ServerApi.Hooks.NetSendData.Register(this, OnSendData, Int32.MaxValue);
-            OTAPI.Hooks.World.IO.PostLoadWorld += OnPostLoadWorld;
-            OTAPI.Hooks.World.IO.PreSaveWorld += OnPreSaveWorld;
-            OTAPI.Hooks.World.IO.PostSaveWorld += OnPostSaveWorld;
         }
 
         #endregion
@@ -137,19 +143,64 @@ namespace FakeProvider
         {
             if (Disposing)
             {
+                Hooks.World.IO.PreLoadWorld -= OnPreLoadWorld;
+                //Hooks.World.IO.PostLoadWorld -= OnPostLoadWorld;
+                Hooks.World.IO.PreSaveWorld -= OnPreSaveWorld;
+                Hooks.World.IO.PostSaveWorld -= OnPostSaveWorld;
                 ServerApi.Hooks.NetSendData.Deregister(this, OnSendData);
-                OTAPI.Hooks.World.IO.PostLoadWorld += OnPostLoadWorld;
-                OTAPI.Hooks.World.IO.PreSaveWorld += OnPreSaveWorld;
-                OTAPI.Hooks.World.IO.PostSaveWorld += OnPostSaveWorld;
             }
             base.Dispose(Disposing);
         }
 
         #endregion
 
+        #region OnPreLoadWorld
+
+        private static HookResult OnPreLoadWorld(ref bool loadFromCloud)
+        {
+            LoadWorldDirect(loadFromCloud);
+            return HookResult.Cancel;
+        }
+
+        #endregion
+        #region OnPostLoadWorld
+
+        private static void OnPostLoadWorld(bool FromCloud)
+        {
+            //LoadCustomTileProvider();
+        }
+
+        #endregion
+        #region OnPreSaveWorld
+
+        private static HookResult OnPreSaveWorld(ref bool Cloud, ref bool ResetTime)
+        {
+            Main.maxTilesX = World.Width;
+            Main.maxTilesY = World.Height;
+            Main.worldSurface -= OffsetY;
+            Main.rockLayer -= OffsetY;
+            Main.tile = World;
+            Tile.HideEntities();
+            return HookResult.Continue;
+        }
+
+        #endregion
+        #region OnPostSaveWorld
+
+        private static void OnPostSaveWorld(bool Cloud, bool ResetTime)
+        {
+            Main.maxTilesX = VisibleWidth;
+            Main.maxTilesY = VisibleHeight;
+            Main.worldSurface += OffsetY;
+            Main.rockLayer += OffsetY;
+            Main.tile = Tile;
+            Tile.UpdateEntities();
+        }
+
+        #endregion
         #region OnSendData
 
-        private void OnSendData(SendDataEventArgs args)
+        private static void OnSendData(SendDataEventArgs args)
         {
             if (args.Handled)
                 return;
@@ -183,12 +234,11 @@ namespace FakeProvider
         }
 
         #endregion
-        #region OnPostLoadWorld
 
-        private void OnPostLoadWorld(bool FromCloud)
+        #region LoadCustomTileProvider
+
+        private static void LoadCustomTileProvider()
         {
-            Console.WriteLine($"REAL maxTilesX, maxTilesY: {Main.maxTilesX}, {Main.maxTilesY}");
-
             if (VisibleWidth < 0)
                 VisibleWidth = (OffsetX + Main.maxTilesX);
             if (VisibleHeight < 0)
@@ -198,10 +248,10 @@ namespace FakeProvider
 
             if (ReadonlyWorld)
                 World = CreateReadonlyTileProvider(WorldProviderName, 0, 0,
-                    Main.maxTilesX, Main.maxTilesY, Main.tile, Int32.MinValue + 1);
+                    Main.maxTilesX, Main.maxTilesY, Int32.MinValue + 1);
             else
                 World = CreateTileProvider(WorldProviderName, 0, 0,
-                    Main.maxTilesX, Main.maxTilesY, Main.tile, Int32.MinValue + 1);
+                    Main.maxTilesX, Main.maxTilesY, Int32.MinValue + 1);
 
             using (IDisposable previous = Main.tile as IDisposable)
             {
@@ -215,35 +265,254 @@ namespace FakeProvider
             WorldGen.setWorldSize();
             GC.Collect();
 
-            // What's with loaded signs, chests and entities? Add to World TileProvider? Nah...
             World.ScanEntities();
         }
 
         #endregion
-        #region OnPreSaveWorld
+        #region LoadWorldDirect
 
-        private HookResult OnPreSaveWorld(ref bool Cloud, ref bool ResetTime)
+        private static void LoadWorldDirect(bool loadFromCloud)
         {
-            Main.maxTilesX = World.Width;
-            Main.maxTilesY = World.Height;
-            Main.worldSurface -= OffsetY;
-            Main.rockLayer -= OffsetY;
-            Main.tile = World;
-            Tile.HideEntities();
-            return HookResult.Continue;
+            WorldFile.IsWorldOnCloud = loadFromCloud;
+            Main.checkXMas();
+            Main.checkHalloween();
+            bool flag = loadFromCloud && SocialAPI.Cloud != null;
+            if (!FileUtilities.Exists(Main.worldPathName, flag) && Main.autoGen)
+            {
+                if (!flag)
+                {
+                    for (int i = Main.worldPathName.Length - 1; i >= 0; i--)
+                    {
+                        if (Main.worldPathName.Substring(i, 1) == (Path.DirectorySeparatorChar.ToString() ?? ""))
+                        {
+                            Directory.CreateDirectory(Main.worldPathName.Substring(0, i));
+                            break;
+                        }
+                    }
+                }
+                WorldGen.clearWorld();
+                Main.ActiveWorldFileData = WorldFile.CreateMetadata((Main.worldName == "") ? "World" : Main.worldName, flag, Main.expertMode);
+                string text = (Main.AutogenSeedName ?? "").Trim();
+                if (text.Length == 0)
+                {
+                    Main.ActiveWorldFileData.SetSeedToRandom();
+                }
+                else
+                {
+                    Main.ActiveWorldFileData.SetSeed(text);
+                }
+                WorldGen.generateWorld(Main.ActiveWorldFileData.Seed, Main.AutogenProgress);
+                WorldFile.saveWorld();
+            }
+            using (MemoryStream memoryStream = new MemoryStream(FileUtilities.ReadAllBytes(Main.worldPathName, flag)))
+            {
+                using (BinaryReader binaryReader = new BinaryReader(memoryStream))
+                {
+                    try
+                    {
+                        WorldGen.loadFailed = false;
+                        WorldGen.loadSuccess = false;
+                        int num = WorldFile.versionNumber = binaryReader.ReadInt32();
+                        int num2;
+                        if (num <= 87)
+                        {
+                            // Not supported
+                            num2 = WorldFile.LoadWorld_Version1(binaryReader);
+                        }
+                        else
+                        {
+                            num2 = LoadWorld_Version2(binaryReader);
+                        }
+                        if (num < 141)
+                        {
+                            if (!loadFromCloud)
+                            {
+                                Main.ActiveWorldFileData.CreationTime = File.GetCreationTime(Main.worldPathName);
+                            }
+                            else
+                            {
+                                Main.ActiveWorldFileData.CreationTime = DateTime.Now;
+                            }
+                        }
+                        binaryReader.Close();
+                        memoryStream.Close();
+                        if (num2 != 0)
+                        {
+                            WorldGen.loadFailed = true;
+                        }
+                        else
+                        {
+                            WorldGen.loadSuccess = true;
+                        }
+                        if (WorldGen.loadFailed || !WorldGen.loadSuccess)
+                        {
+                            return;
+                        }
+                        WorldGen.gen = true;
+                        WorldGen.waterLine = Main.maxTilesY;
+                        Liquid.QuickWater(2, -1, -1);
+                        WorldGen.WaterCheck();
+                        int num3 = 0;
+                        Liquid.quickSettle = true;
+                        int num4 = Liquid.numLiquid + LiquidBuffer.numLiquidBuffer;
+                        float num5 = 0f;
+                        while (Liquid.numLiquid > 0 && num3 < 100000)
+                        {
+                            num3++;
+                            float num6 = (float)(num4 - (Liquid.numLiquid + LiquidBuffer.numLiquidBuffer)) / (float)num4;
+                            if (Liquid.numLiquid + LiquidBuffer.numLiquidBuffer > num4)
+                            {
+                                num4 = Liquid.numLiquid + LiquidBuffer.numLiquidBuffer;
+                            }
+                            if (num6 > num5)
+                            {
+                                num5 = num6;
+                            }
+                            else
+                            {
+                                num6 = num5;
+                            }
+
+                            SetStatusText(string.Concat(new object[]
+                            {
+                                Lang.gen[27].Value,
+                                " ",
+                                (int)(num6 * 100f / 2f + 50f),
+                                "%"
+                            }));
+                            Liquid.UpdateLiquid();
+                        }
+                        Liquid.quickSettle = false;
+                        Main.weatherCounter = WorldGen.genRand.Next(3600, 18000);
+                        Cloud.resetClouds();
+                        WorldGen.WaterCheck();
+                        WorldGen.gen = false;
+                        NPC.setFireFlyChance();
+                        Main.InitLifeBytes();
+                        if (Main.slimeRainTime > 0.0)
+                        {
+                            Main.StartSlimeRain(false);
+                        }
+                        NPC.setWorldMonsters();
+                    }
+                    catch (Exception value)
+                    {
+                        WorldGen.loadFailed = true;
+                        WorldGen.loadSuccess = false;
+                        System.Console.WriteLine(value);
+                        try
+                        {
+                            binaryReader.Close();
+                            memoryStream.Close();
+                        }
+                        catch
+                        {
+                        }
+                        return;
+                    }
+                }
+            }
+
+            EventInfo eventOnWorldLoad = typeof(WorldFile).GetEvent("OnWorldLoad", BindingFlags.Public | BindingFlags.Static);
+            eventOnWorldLoad.GetRaiseMethod()?.Invoke(null, new object[] { });
+            //if (WorldFile.OnWorldLoad != null)
+                //WorldFile.OnWorldLoad();
         }
 
         #endregion
-        #region OnPostSaveWorld
+        #region SetStatusText
 
-        private void OnPostSaveWorld(bool Cloud, bool ResetTime)
+        private static void SetStatusText(string text)
         {
-            Main.maxTilesX = VisibleWidth;
-            Main.maxTilesY = VisibleHeight;
-            Main.worldSurface += OffsetY;
-            Main.rockLayer += OffsetY;
-            Main.tile = Tile;
-            Tile.UpdateEntities();
+            Hooks.Game.StatusTextHandler statusTextWrite = Hooks.Game.StatusTextWrite;
+            HookResult? hookResult = (statusTextWrite != null) ? new HookResult?(statusTextWrite(ref text)) : null;
+            bool flag = hookResult != null && hookResult.Value == HookResult.Cancel;
+            if (!flag)
+            {
+                typeof(Main).GetField("statusText", BindingFlags.NonPublic | BindingFlags.Static).SetValue(null, text);
+            }
+        }
+
+        #endregion
+        #region LoadWorld_Version2
+
+        private static int LoadWorld_Version2(BinaryReader reader)
+        {
+            reader.BaseStream.Position = 0L;
+            bool[] importance;
+            int[] array;
+            if (!WorldFile.LoadFileFormatHeader(reader, out importance, out array))
+            {
+                return 5;
+            }
+            if (reader.BaseStream.Position != (long)array[0])
+            {
+                return 5;
+            }
+            WorldFile.LoadHeader(reader);
+            if (reader.BaseStream.Position != (long)array[1])
+            {
+                return 5;
+            }
+
+            LoadCustomTileProvider();
+
+            WorldFile.LoadWorldTiles(reader, importance);
+            if (reader.BaseStream.Position != (long)array[2])
+            {
+                return 5;
+            }
+            WorldFile.LoadChests(reader);
+            if (reader.BaseStream.Position != (long)array[3])
+            {
+                return 5;
+            }
+            WorldFile.LoadSigns(reader);
+            if (reader.BaseStream.Position != (long)array[4])
+            {
+                return 5;
+            }
+            WorldFile.LoadNPCs(reader);
+            if (reader.BaseStream.Position != (long)array[5])
+            {
+                return 5;
+            }
+            if (WorldFile.versionNumber >= 116)
+            {
+                if (WorldFile.versionNumber < 122)
+                {
+                    WorldFile.LoadDummies(reader);
+                    if (reader.BaseStream.Position != (long)array[6])
+                    {
+                        return 5;
+                    }
+                }
+                else
+                {
+                    WorldFile.LoadTileEntities(reader);
+                    if (reader.BaseStream.Position != (long)array[6])
+                    {
+                        return 5;
+                    }
+                }
+            }
+            if (WorldFile.versionNumber >= 170)
+            {
+                WorldFile.LoadWeightedPressurePlates(reader);
+                if (reader.BaseStream.Position != (long)array[7])
+                {
+                    return 5;
+                }
+            }
+            if (WorldFile.versionNumber >= 189)
+            {
+                WorldFile.LoadTownManager(reader);
+                if (reader.BaseStream.Position != (long)array[8])
+                {
+                    return 5;
+                }
+            }
+            return WorldFile.LoadFooter(reader);
         }
 
         #endregion
